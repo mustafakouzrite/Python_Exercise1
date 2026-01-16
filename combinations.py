d55aabdbec859315822759db02ac0459f328307d
mustafakouzrite <mustafakouzrite@gmail.com>
2026-01-16 16:34:53 +0100

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -1,10 +1,10 @@
 def fact(n):
-    result = 1
-    for i in range(1, n + 1):
-        result = result * i
-    return result
+    if n == 0 or n == 1:
+        return 1
+    return n * fact(n - 1)
 
 
 def comb(n, k):
     return fact(n) // (fact(k) * fact(n - k))
-With fact Iterative function 
+    With fact Recursive function
+    
