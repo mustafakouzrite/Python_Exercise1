5a67cf4769228d61d1d8c9aa381892980733cd78
mustafakouzrite <mustafakouzrite@gmail.com>
2026-01-16 16:31:11 +0100

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -1,3 +1,9 @@
+def fact(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
+
+
 def comb(n, k):
-    return 1
-    Trivial comb
+    return fact(n) // (fact(k) * fact(n - k))
